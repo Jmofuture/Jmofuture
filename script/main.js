d01da2811d0607d41916f87aee9e54711c85cfef
Jean Olmedillo <jmof.html@gmail.com>
2022-01-08 17:01:37 -0300

    act. index

--- script/main.js
+++ script/main.js
@@ -3,9 +3,12 @@
 
 let titleAbout = document.getElementById("demo")
 
-console.log(titleAbout)
+console.log(titleAbout )
 
 titleAbout.addEventListener("mouseover", () =>{
+    console.log('Hola Mouse')
+    document.getElementById('demo').classList.add('anchorHide');
+    document.getElementById('anchorWork').classList.add('anchorShow')
+
 })
 
-titleAbout();
