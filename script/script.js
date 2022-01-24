83108c4e4009c164efbde3c889fc76aac1a7beb4
Jean Olmedillo <jmof.html@gmail.com>
2022-01-23 22:35:14 -0300

    rediseño

---
+++ script/script.js
@@ -0,0 +1,23 @@
+const $form = document.querySelector('#form')
+
+console.log($form);
+
+$form.addEventListener('submit', handleSubmit)
+
+async function handleSubmit(event) {
+    event.preventDefault();
+    let status = document.getElementById("status");
+    let data = new FormData(event.target);
+    fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        status.textContent = "Gracias, tu mensaje fue enviado!";
+        form.reset()
+    }).catch(error => {
+        status.textContent = "Oops! Hubo un problema, tu mensaje no fue enviado"
+    });
+}
